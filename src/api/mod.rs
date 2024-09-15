e31d84813b86fa83ffbc1915b635ea30eaac4cdf
Eveheeero <xhve00000@naver.com>
2024-09-15 16:41:45 +0900

    +

---
+++ src/api/mod.rs
@@ -0,0 +1,3 @@
+pub(crate) mod assistants;
+pub(crate) mod messages;
+pub(crate) mod threads;
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![cfg_attr(not(test), windows_subsystem = "windows")]
+
+mod api;
+mod automation;
+mod fonts;
+use eframe::{egui, App};
+
+fn main() -> eframe::Result<()> {
+    let mut native_options = eframe::NativeOptions::default();
+    native_options.viewport.maximized = Some(true);
+    eframe::run_native(
+        "OpenAI API Assistants",
+        native_options,
+        Box::new(|cc| Ok(Box::new(Assistants::new(cc)))),
+    )
+}
+
+struct Assistants {
+    api_key: String,
+}
+
+impl Assistants {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // 폰트 설정
+        cc.egui_ctx.set_fonts(fonts::get_fonts());
+
+        let db = sled::open(".assistants").unwrap();
+        let api_key = db.get("api_key").unwrap().unwrap_or_default();
+        let api_key = String::from_utf8(api_key.to_vec()).unwrap();
+
+        Self { api_key }
+    }
+}
+impl App for Assistants {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
+}
